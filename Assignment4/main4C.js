1f17509dc9fa9fbe369da7d394c66689bd4d67eb
EddisonDC <eddison.jimenezsantana@dcmail.ca>
2023-11-22 11:27:55 -0500

    Changes to assignment 4 part 3
    Changes to assignment 4 part 3 in the following files:
    - Assignment4C.html
    - main4C.js

--- Assignment4/main4C.js
+++ Assignment4/main4C.js
@@ -3,7 +3,6 @@
  */
 
 // setup canvas
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -11,14 +10,104 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
-function random(min, max) {
+function random(min, max) 
+{
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
 
 // function to generate random color
-
-function randomRGB() {
+function randomRGB() 
+{
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// Class to give features to the balls
+class Ball 
+{
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  // Method to draw the balls in the screen
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  // Method to give speed to the balls
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  // Function to change balls colour when they cross each other
+  collisionDetect() {
+  for (const ball of balls) {
+    if (this !== ball) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.color = this.color = randomRGB();
+      }
+    }
+  }
+}
+}
+
+// Create 25 balls in the screen and push them
+const balls = [];
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+// Give all features to each ball
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
